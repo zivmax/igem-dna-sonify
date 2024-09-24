311565224ef39dd10c990a9d8b50c7580d220aba
zivmax <zivmax@foxmail.com>
2024-09-24 22:21:39 +0800

    fix: Fix 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import * as React from 'react';
 import Layout from '../components/layout';
-import { Ghost } from 'react-kawaii';
 import styled from 'styled-components';
+import StyledLink from '../components/styled-link';
+
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+}
+
+const paragraphStyles = {
+  marginBottom: 48,
+}
+
 
 const NotFoundPage = () => {
   return (
     <Layout title="404 Not Found">
       <NotFoundPageWrapper>
-        <Ghost size={240} mood="sad" color="#E0E4E8" />
-        <h1>OOPS..</h1>
-        <p>404. Page not found</p>
+      <h1 style={headingStyles}>Page not found</h1>
+      <p style={paragraphStyles}>
+        Sorry 😔, we couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br />
+            Try creating a page in <code className='language-text'>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
+        <br />
+        <br />
+        <StyledLink to="/">Go home</StyledLink>
+      </p>
       </NotFoundPageWrapper>
     </Layout>
   );
